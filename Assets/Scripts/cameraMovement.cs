00f5a0766fd69269d2ac82a202987ac2ae1e9afc
Kazey974 <denis.lamyam@live.fr>
2018-08-07 21:34:04 +0400

    Player movement + Camera movement

---
+++ Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour {
+
+    public GameObject anchor;
+    public float speed;
+    private Vector3 targetPos;
+
+	// Use this for initialization
+	void Start () {
+        speed = 0.05f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetPos = new Vector3(anchor.transform.position.x,5f,anchor.transform.position.z-10f);
+        transform.position = Vector3.Lerp(transform.position, targetPos, speed);
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public float mvSpeed;
+
+	// Use this for initialization
+	void Start () {
+		mvSpeed = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Z)){ transform.position += Vector3.forward*mvSpeed*Time.deltaTime;}
+		if(Input.GetKey(KeyCode.Q)){ transform.position += Vector3.left*mvSpeed*Time.deltaTime;   }
+		if(Input.GetKey(KeyCode.S)){ transform.position += Vector3.back*mvSpeed*Time.deltaTime;   }
+		if(Input.GetKey(KeyCode.D)){ transform.position += Vector3.right*mvSpeed*Time.deltaTime;  }
+	}
+}
